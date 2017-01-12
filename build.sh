f148abe3f8e286bfaefce0442405b21a393bc477
Alex Beregszaszi <alex@rtfs.hu>
2017-01-12 02:00:24 +0000

    Include git hash in generated PDF

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+SHA=`git rev-parse --short --verify HEAD`
+
+echo "\newcommand{\YellowPaperVersionNumber}{$SHA}" > Version.tex
+
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
 bibtex Paper && \
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
