ac6b93859c6c30dd4eb8786df356b0d9bd789280
Alex Beregszaszi <alex@rtfs.hu>
2017-01-12 14:49:25 +0000

    Proper error propagation in build script

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-pdflatex -interaction=errorstopmode -halt-on-error Paper.tex
-bibtex Paper
-pdflatex -interaction=errorstopmode -halt-on-error Paper.tex
-pdflatex -interaction=errorstopmode -halt-on-error Paper.tex
+pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
+bibtex Paper && \
+pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
+pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex
