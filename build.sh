8b25783b6bfe1a971f0671eedcba3b2ac1aa4afd
Alex Beregszaszi <alex@rtfs.hu>
2017-01-12 11:06:48 +0000

    Add build date

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 set -e
 
 SHA=`git rev-parse --short --verify HEAD`
+DATE=`date "+%Y-%m-%d"`
 
-echo "\newcommand{\YellowPaperVersionNumber}{$SHA}" > Version.tex
+echo "\newcommand{\YellowPaperVersionNumber}{$SHA - $DATE}" > Version.tex
 
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
 bibtex Paper && \
