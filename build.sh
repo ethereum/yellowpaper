7a9a385a4e89303a9351c27939cfa0c698628d0c
William Entriken <github.com@phor.net>
2018-02-12 16:00:22 -0500

    Add option to build white paper

--- build.sh
+++ build.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env bash
 
+##
+## Usage: build.sh [white]
+##
+
+
 set -e
+
+rm -rf Options.tex
 
 if [ -d ".git" ]; then
 
 SHA=`git rev-parse --short --verify HEAD`
 DATE=`git show -s --format="%cd" --date=short HEAD`
 REV="$SHA - $DATE"
-
-else
-
-REV="unknown revision"
+echo "\def\YellowPaperVersionNumber{$REV}" >> Options.tex
 
 fi
+
+
+if [ "$1" == "white" ]; then
+
+echo "\definecolor{pagecolor}{rgb}{1,1,1}" >> Options.tex
+
+fi
+
+
 
 echo "\newcommand{\YellowPaperVersionNumber}{$REV}" > Version.tex
 
@@ -21,3 +34,4 @@ bibtex Paper && \
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex && \
 pdflatex -interaction=errorstopmode -halt-on-error Paper.tex
+rm -rf Options.tex
